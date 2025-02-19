71ff2a734048224bde19fe7031d01826bf314073
Aman Kaushik <kaushikaman239@gmail.com>
2025-02-19 18:26:16 +0530

    add styiling

--- src/componenet/Twodiv.jsx
+++ src/componenet/Twodiv.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Two.css'
 
 function Twodiv() {
   return (
-    <div>
-        
+    <div className='two-div'>
+      <div className='dev-div'>
+        <div className='left-div'>
+          <span style={{fontSize:'13px', color:'#bbb'}}>Built By Developers</span>
+            <h3>Purity UI Dashboard</h3>
+            <p>From colors, cards, typography to complex elements, you will find the full documentation.</p>
+            
+            <div className="down-btn">
+                <a href="#">Read More</a>
+            </div>
+        </div>
+        <div>
+            <div className="right-dev"></div>
+        </div>
+      </div>
     </div>
   )
 }
